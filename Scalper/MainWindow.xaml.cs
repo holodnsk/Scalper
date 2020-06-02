64e926a3f91a0a33e546291c5b8465891850c10b
holodnsk <holodnsk@gmail.com>
2020-06-02 22:18:06 +0700

    save

--- Scalper/MainWindow.xaml.cs
+++ Scalper/MainWindow.xaml.cs
@@ -2,10 +2,11 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
+using System.Linq;
 using System.Reflection;
 using System.Runtime.Serialization.Formatters.Binary;
+using ddfplus;
 using Ecng.Common;
-using Ecng.Reflection;
 using Ecng.Xaml;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
@@ -14,6 +15,7 @@ using StockSharp.Algo;
 using StockSharp.BusinessEntities;
 using StockSharp.Messages;
 using StockSharp.SmartCom;
+using JsonSerializer = RestSharp.Serializers.JsonSerializer;
 
 namespace Scalper
 {
@@ -88,6 +90,8 @@ namespace Scalper
         private void SubscribeEvents()
         {
             _trader.ReConnectionSettings.WorkingTime = ExchangeBoard.Forts.WorkingTime;
+            // _trader.NewMessage +=  message => this.GuiAsync(NewMessageEventHandler(message));
+            _trader.OutMessageChannel.NewOutMessage +=  message => this.GuiAsync(NewMessageEventHandler(message));
             _trader.Restored += () => this.GuiAsync(RestoredEventHandler);
             _trader.Connected += () => this.GuiAsync(ConnectedEventHandler);
             _trader.Disconnected += () => this.GuiAsync(DisconnectedEventHandler);
@@ -119,108 +123,113 @@ namespace Scalper
                 this.GuiAsync(() => MassOrderCancelFailedEventHandler(transId, error));
         }
 
-        private void NewOrderLogItemHandler(OrderLogItem newOrderLogItem)
+        private Action NewMessageEventHandler(Message message)
         {
-            WriteTraffic(MethodBase.GetCurrentMethod().Name, newOrderLogItem);
+            WriteTraffic(message);
+            return null;
         }
 
+        private void NewOrderLogItemHandler(OrderLogItem newOrderLogItem)
+        {
+            //WriteTraffic(MethodBase.GetCurrentMethod().Name, newOrderLogItem);
+        }
 
         private void NewMyTradeEventHandler(MyTrade newMyTrade)
         {
-            WriteTraffic(MethodBase.GetCurrentMethod().Name, newMyTrade);
+            // WriteTraffic(MethodBase.GetCurrentMethod().Name, newMyTrade);
         }
 
         private void MarketDataSubscriptionFailedEventHandler(Security security, MarketDataMessage msg, Exception error)
         {
-            WriteTraffic(MethodBase.GetCurrentMethod().Name, security, msg,
-                error);
+            // WriteTraffic(MethodBase.GetCurrentMethod().Name, security, msg,
+                // error);
         }
 
         private void DisconnectedEventHandler()
         {
-            WriteTraffic(MethodBase.GetCurrentMethod().Name);
+            // WriteTraffic(MethodBase.GetCurrentMethod().Name);
         }
 
         private void MassOrderCancelFailedEventHandler(long transId, Exception error)
         {
-            WriteTraffic(MethodBase.GetCurrentMethod().Name, transId, error);
+            // WriteTraffic(MethodBase.GetCurrentMethod().Name, transId, error);
         }
 
         private void StopOrderCancelFailedEventHandler(OrderFail stopOrderCancelFailed)
         {
-            WriteTraffic(MethodBase.GetCurrentMethod().Name,
-                stopOrderCancelFailed);
+            // WriteTraffic(MethodBase.GetCurrentMethod().Name,
+                // stopOrderCancelFailed);
         }
 
         private void StopOrderRegisterFailedEventHandler(OrderFail stopOrderRegisterFailed)
         {
-            WriteTraffic(MethodBase.GetCurrentMethod().Name,
-                stopOrderRegisterFailed);
+            // WriteTraffic(MethodBase.GetCurrentMethod().Name,
+                // stopOrderRegisterFailed);
         }
 
         private void OrderCancelFailedEventHandler(OrderFail orderCancelFailed)
         {
-            WriteTraffic(MethodBase.GetCurrentMethod().Name, orderCancelFailed);
+            // WriteTraffic(MethodBase.GetCurrentMethod().Name, orderCancelFailed);
         }
 
         private void OrderRegisterFailedEventHandler(OrderFail orderRegisterFailed)
         {
-            WriteTraffic(MethodBase.GetCurrentMethod().Name, orderRegisterFailed);
+            // WriteTraffic(MethodBase.GetCurrentMethod().Name, orderRegisterFailed);
         }
 
         private void NewPositionEventHandler(Position newPosition)
         {
-            WriteTraffic(MethodBase.GetCurrentMethod().Name, newPosition);
+            // WriteTraffic(MethodBase.GetCurrentMethod().Name, newPosition);
         }
 
         private void NewPortfolioEventHandler(Portfolio newPortfolio)
         {
-            WriteTraffic(MethodBase.GetCurrentMethod().Name, newPortfolio);
+            // WriteTraffic(MethodBase.GetCurrentMethod().Name, newPortfolio);
         }
 
         private void NewStopOrderEventHandler(Order newStopOrder)
         {
-            WriteTraffic(MethodBase.GetCurrentMethod().Name, newStopOrder);
+            // WriteTraffic(MethodBase.GetCurrentMethod().Name, newStopOrder);
         }
 
         private void NewOrderEventHandler(Order newOrder)
         {
-            WriteTraffic(MethodBase.GetCurrentMethod().Name, newOrder);
+            // WriteTraffic(MethodBase.GetCurrentMethod().Name, newOrder);
         }
 
         private void NewTradeEventHandler(Trade newTrade)
         {
-            WriteTraffic(MethodBase.GetCurrentMethod().Name, newTrade);
+            // WriteTraffic(MethodBase.GetCurrentMethod().Name, newTrade);
         }
 
         private void RestoredEventHandler()
         {
-            WriteTraffic(MethodBase.GetCurrentMethod().Name);
+            // WriteTraffic(MethodBase.GetCurrentMethod().Name);
         }
 
         private void ConnectionErrorEventHandler(Exception error)
         {
-            WriteTraffic(MethodBase.GetCurrentMethod().Name, error);
+            // WriteTraffic(MethodBase.GetCurrentMethod().Name, error);
         }
 
         private void ConnectedEventHandler()
         {
-            WriteTraffic(MethodBase.GetCurrentMethod().Name);
+            // WriteTraffic(MethodBase.GetCurrentMethod().Name);
         }
 
         private void TransactionErrorEventHandler(Exception error)
         {
-            WriteTraffic(MethodBase.GetCurrentMethod().Name, error);
+            // WriteTraffic(MethodBase.GetCurrentMethod().Name, error);
         }
 
         private void MarketDepthsChangedEventHandler(IEnumerable<MarketDepth> changedMarketDepths)
         {
-            WriteTraffic(MethodBase.GetCurrentMethod().Name, changedMarketDepths);
+            // WriteTraffic(MethodBase.GetCurrentMethod().Name, changedMarketDepths);
         }
 
         private void MarketDepthChangedEventHandler(MarketDepth changedMarketDepth)
         {
-            WriteTraffic(MethodBase.GetCurrentMethod().Name, changedMarketDepth);
+            // WriteTraffic(MethodBase.GetCurrentMethod().Name, changedMarketDepth);
             if (changedMarketDepth.Security.Name=="AFLT")
             {
                 AFLT.NewMarketDepth(changedMarketDepth);
@@ -231,7 +240,7 @@ namespace Scalper
         {
             if (CfgTradingSecurity(security))
             {
-                WriteTraffic(MethodBase.GetCurrentMethod().Name, security);
+                // WriteTraffic(MethodBase.GetCurrentMethod().Name, security);
                 _trader.RegisterMarketDepth(security);
                 _trader.RegisterTrades(security);
                 _trader.RegisterOrderLog(security);
@@ -247,33 +256,35 @@ namespace Scalper
 
                 JObject jObject = JObject.Parse(line);
 
-                string trafficEventHandlerName = jObject.Property("trafficEventHandlerName").ToString()
-                    .Replace("trafficEventHandlerName", "")
-                    .Replace("\"", "")
-                    .Replace(" ", "")
-                    .Replace(":", "");
+                var messageTypes = typeof(ConnectMessage).Assembly.GetTypes().Where((t) => t.FullName == jObject.GetValue("ObjectType").ToString());
+                foreach (Type messageType in messageTypes)
+                {
+                    string objectToDeserialize = jObject.GetValue("message").ToString();
+                    
+                    using (MemoryStream memoryStream = new MemoryStream())
+                    {
+                        BinaryReader readFile = new BinaryReader(new FileStream(objectToDeserialize, FileMode.Open));
+                        byte[] readBytes = readFile.ReadBytes((int) new FileInfo(objectToDeserialize).Length + 1);
 
-                MethodInfo methodInfo = GetType().GetMethod(trafficEventHandlerName,BindingFlags.NonPublic | BindingFlags.Instance);
-
-                Debug.Assert(methodInfo != null, nameof(methodInfo) + " != null");
-                methodInfo.Invoke(this,GetParametersForMethodFromFile(methodInfo, jObject));
+                        var memoryWriter = new BinaryWriter(memoryStream);
+                        memoryWriter.Write(readBytes);
+                        memoryWriter.Flush();
+                        memoryStream.Position = 0;
+                        var binaryFormatter = new BinaryFormatter();
+                        object deserializedObject = binaryFormatter.Deserialize(memoryStream);
+                        
+                    }
+                    
+                    Message deserializeObject = (Message) JsonConvert.DeserializeObject(objectToDeserialize,messageType);
+                    _trader.SendInMessage(deserializeObject);                    
+                }
+                
             }
         }
 
         private object[] GetParametersForMethodFromFile(MethodInfo methodInfo, JObject jObject)
         {
-            var enumerable = jObject.Values<object>();
-            int count = 0;
-            foreach (object o in enumerable)
-            {
-                if (count==0)
-                    continue;
-                count++;
-                string objectType = o.ToString();
-                object objectFromFile = readObjectFromFile(jObject, objectType);
-            }
             ParameterInfo[] parametersFromMethodInfo = methodInfo.GetParameters();
-            Type[] parameterTypes = methodInfo.GetParameterTypes();
             object[] resultParameters = new object[parametersFromMethodInfo.Length];
             int countParameter = 0;
             foreach (var parameter in parametersFromMethodInfo)
@@ -287,46 +298,9 @@ namespace Scalper
             return resultParameters;
         }
 
-        
-
-        private void WriteTraffic(string trafficEventHandlerName, params object[] objectsForWrite)
-        {
-            if (_trafficMode != TrafficMode.Write)
-                return;
-
-            trafficEntitiesCounter.Text = (Int32.Parse(trafficEntitiesCounter.Text)+1).ToString();
-            TextWriter textWriter = new StringWriter();
-            JsonWriter jsonWriter = new JsonTextWriter(textWriter);
-            jsonWriter.WriteStartObject();
-            jsonWriter.WritePropertyName("trafficEventHandlerName");
-            jsonWriter.WriteValue(trafficEventHandlerName);
-
-            foreach (var objectForWrite in objectsForWrite)
-            {
-                string name = objectForWrite.GetType().ToString();
-                jsonWriter.WritePropertyName(name);
-                using (MemoryStream memoryStream = new MemoryStream())
-                {
-                    BinaryFormatter formatter = new BinaryFormatter();
-                    formatter.Serialize(memoryStream, objectForWrite);
-                    byte[] array = memoryStream.ToArray();
-                    string filename = "traffic\\"+DateTime.Now.Ticks;
-                    BinaryWriter file = new BinaryWriter(new FileStream(filename,FileMode.Create));
-                    file.Write(array);
-                    file.Close();
-                    jsonWriter.WriteValue(filename);
-                }
-            }
-            jsonWriter.WriteEndObject();
-            _trafficFile.Write(textWriter);
-            _trafficFile.Write("\n");
-            _trafficFile.Flush();
-        }
-
         private object readObjectFromFile(JObject jObject, string objectType)
         {
-            JProperty jProperty = jObject.Property(objectType);
-            string filename = jProperty.ToString().
+            string filename = jObject.Property(objectType).ToString().
                 Replace("\"","").
                 Replace(":","").
                 Replace(" ","").
@@ -346,6 +320,53 @@ namespace Scalper
                 return deserializedObject;
             }
         }
+
+        private void WriteTraffic(Message message)
+        {
+            if (_trafficMode != TrafficMode.Write)
+                return;
+
+            string serializeObject = new JsonSerializer().Serialize(message.Clone());
+            
+            if (message.Type==MessageTypes.Time)
+            {
+                return;
+            }
+            if (serializeObject.Contains("SecurityCode"))
+            {
+                if (!serializeObject.Contains("AFLT"))
+                {
+                    return;
+                }
+            }
+            
+            //trafficEntitiesCounter.Text = (Int32.Parse(trafficEntitiesCounter.Text)+1).ToString();
+            TextWriter textWriter = new StringWriter();
+            JsonWriter jsonWriter = new JsonTextWriter(textWriter);
+            jsonWriter.WriteStartObject();
+
+            jsonWriter.WritePropertyName("ObjectType");
+            jsonWriter.WriteValue(message.GetType().ToString());
+            jsonWriter.WritePropertyName("message");
+
+            using (MemoryStream memoryStream = new MemoryStream())
+            {
+                BinaryFormatter formatter = new BinaryFormatter();
+                formatter.Serialize(memoryStream, serializeObject);
+                byte[] array = memoryStream.ToArray();
+                string filename = "traffic\\"+DateTime.Now.Ticks;
+                BinaryWriter file = new BinaryWriter(new FileStream(filename,FileMode.Create));
+                file.Write(array);
+                file.Close();
+                jsonWriter.WriteValue(filename);
+            }
+
+            jsonWriter.WriteEndObject();
+            _trafficFile.Write(textWriter);
+            _trafficFile.Write("\n");
+            _trafficFile.Flush();
+        }
+
         private readonly TrafficMode _trafficMode = TrafficMode.Read;
     }
 }
