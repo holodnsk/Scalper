1a21cff2b1b5eef489fb632238bfa2357eaca80f
holodnsk <holodnsk@gmail.com>
2020-06-13 12:32:27 +0700

    density container

---
+++ Scalper/Density.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using AD.Common.DataStructures;
+using DevExpress.Mvvm.Native;
+
+namespace Scalper
+{
+    public class Density
+    {
+        public decimal Price { get; set; }
+        public List<decimal> Values { get; set; }
+        public decimal MaxValue { get; set; }
+        
+        public OrderDirection Direction { get; set; }
+        
+        public bool Equals(Density density)
+        {
+            if (density == null)
+                return false;
+            return Price==density.Price && Direction==density.Direction;
+        }
+
+        public int GetHashCode()
+        {
+            return Price.GetHashCode()+Values.GetHashCode()+MaxValue.GetHashCode();
+        }
+    }
+}
---
+++ Scalper/DensityesContainer.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace Scalper
+{
+    public class DensitiesContainer
+    {
+        public HashSet<Density> Densities { get; set; }
+    }
+}
