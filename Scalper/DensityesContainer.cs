fa88f9be404c44dc0f66c22d0ce4071b838874c7
holodnsk <holodnsk@gmail.com>
2020-06-14 19:22:58 +0700

    fix crashing when get deleted density and refactoring

--- Scalper/DensityesContainer.cs
+++ Scalper/DensityesContainer.cs
@@ -1,41 +1,35 @@
 ﻿using System;
 using System.Collections.Generic;
-using Ecng.Reflection;
 using StockSharp.BusinessEntities;
 
 namespace Scalper
 {
     public class DensitiesContainer
     {
-        public DensitiesContainer()
-        {
-            Densities = new Dictionary<decimal, Density>();
-        }
-
-        private Dictionary<decimal,Density> Densities;
+        private readonly Dictionary<decimal,Density> _densities = new Dictionary<decimal, Density>();
         private const Decimal SignificantVolume = 2000; // TODO config this
 
         public void HandleValue(Quote quote)
         {
-            var IsShouldRenewMaxVolume = Densities.ContainsKey(quote.Price) && Densities[quote.Price].MaxVolume < quote.Volume;
-            var isDensityBeingTooSmall = Densities.ContainsKey(quote.Price) && (quote.Volume<Densities[quote.Price].MaxVolume/3 && quote.Volume<SignificantVolume);
-            var IsShouldRenewVolumeOfDensity = Densities.ContainsKey(quote.Price) && !(quote.Volume<Densities[quote.Price].MaxVolume/3 && quote.Volume<SignificantVolume);
-            var IsNewDensity = quote.Volume>=SignificantVolume && !Densities.ContainsKey(quote.Price);
+            var isShouldRenewMaxVolume = _densities.ContainsKey(quote.Price) && _densities[quote.Price].MaxVolume < quote.Volume;
+            var isDensityBeingTooSmall = _densities.ContainsKey(quote.Price) && (quote.Volume<_densities[quote.Price].MaxVolume/3 && quote.Volume<SignificantVolume);
+            var isShouldRenewVolumeOfDensity = _densities.ContainsKey(quote.Price) && quote.Volume!=_densities[quote.Price].Volume;
+            var isNewDensity = quote.Volume>=SignificantVolume && !_densities.ContainsKey(quote.Price);
             
-            if (IsShouldRenewMaxVolume)
-                Densities[quote.Price].MaxVolume = quote.Volume;
+            if (isShouldRenewMaxVolume)
+                _densities[quote.Price].MaxVolume = quote.Volume;
 
             if (isDensityBeingTooSmall)
             {
-                Densities.Remove(quote.Price);
+                _densities.Remove(quote.Price);
                 // TODO event for opened position and order if have for action                        
             }
 
-            if (IsShouldRenewVolumeOfDensity)
-                Densities[quote.Price].Volume = quote.Volume;
+            if (isShouldRenewVolumeOfDensity && !isDensityBeingTooSmall)
+                _densities[quote.Price].Volume = quote.Volume;
 
-            if (IsNewDensity)
-                Densities.Add(quote.Price,new Density(quote.Price,quote.Volume,quote.Volume,quote.OrderDirection));
+            if (isNewDensity)
+                _densities.Add(quote.Price,new Density(quote.Price,quote.Volume,quote.Volume,quote.OrderDirection));
         }
     }
 }
